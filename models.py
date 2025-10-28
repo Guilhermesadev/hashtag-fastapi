e3c586e2519cb6768fcea0a1526b429d4f18bce8
Guilherme santos de araujo <guilherme.satech@gmail.com>
2025-10-28 11:04:13 -0300

    add alembic

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 # criar as classes para o banco de dados
 from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
 from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
 
 # Criar a conexão com o banco
 db = create_engine("sqlite:///banco.db")
@@ -18,9 +19,53 @@ class Usuario(Base):
     email = Column("email", String, nullable=False)
     senha = Column("senha", String)
     ativo = Column("ativo", Boolean)
-    admin = Column("admin", Boolean)
+    admin = Column("admin", Boolean, default=False)
 
+
+    def __init__(self, nome, email, senha, ativo=True, admin=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
 
 #Pedido
+class Pedido(Base):
+    __tablename__ = "pedidos"
+
+    # STATUS_PEDIDOS = (
+    #     ("PENDENTE", "PENDENTE"),
+    #     ("CANCELADO", "CANCELADO"),
+    #     ("FINALIZADO", "FINALIZADO")
+    # )
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", String) # pendente, cancelado, finalizado
+    usuario = Column("usuario", ForeignKey("usuarios.id"))
+    preco = Column("preco", Float)
+    # itens =
+
+    def __init__(self, usuario, status="PENDENTE", preco=0):
+        self.usuario = usuario
+        self.preco = preco
+        self.status = status
+
 #ItensPedido
+class ItemPedido(Base):
+    __tablename__ = "itens_pedido"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    qtd = Column("quantidada", Integer)
+    sabor = Column("sabor", String)
+    tamanho = Column("tamanho",String)
+    preco_uni = Column("preco_unitario", Float)
+    pedido = Column("pedido", ForeignKey("pedidos.id"))
+
+    def __init__(self, qtd, sabor, tamanho, preco_uni, pedido):
+        self.qtd = qtd
+        self.sabor = sabor
+        self.tamanho = tamanho
+        self.preco_uni = preco_uni
+        self.pedido = pedido
+
 #Executa a criação dos metadados do seu banco (cria efetivamente o banco de dados)
