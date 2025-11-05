8470e049788d59f88e7500788dd7d5cc0d8ef78e
Guilherme santos de araujo <guilherme.satech@gmail.com>
2025-11-04 22:40:38 -0300

    uptade in tokens

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from fastapi import FastAPI #importa
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
 from dotenv import load_dotenv
 import os
 
@@ -10,12 +11,13 @@ from sqlalchemy.util import deprecated
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGOTITHM")
+ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 app = FastAPI() #Extanciar o fastapi
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/login-form")
 
 from auth_routes import auth_router
 from order_routes import order_router
